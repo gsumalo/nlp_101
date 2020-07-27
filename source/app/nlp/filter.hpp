3511c638b0788d8c276f6d67d41ea3df4c549122
Jesus Malo Poyatos <jmalopoy@opentext.com>
2020-07-27 18:31:39 +0200

    Addition of forgotten files

---
+++ source/app/nlp/filter.hpp
@@ -0,0 +1,12 @@
+#ifndef NLP_FILTER_HPP_
+#define NLP_FILTER_HPP_
+
+#include <iostream>
+
+namespace nlp {
+
+void filter(std::istream & input, std::ostream & output);
+
+}   // namespace nlp
+
+#endif // NLP_FILTER_HPP_
