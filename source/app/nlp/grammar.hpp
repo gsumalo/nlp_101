27c99e573750d310542db2de028130672c6fa8ee
Jesus Malo Poyatos <jmalopoy@opentext.com>
2020-07-28 17:59:35 +0200

    Addition of minimal semantic filtering

--- source/app/nlp/grammar.hpp
+++ source/app/nlp/grammar.hpp
@@ -1,8 +1,11 @@
 #ifndef NLP_GRAMMAR_HPP_
 #define NLP_GRAMMAR_HPP_
 
+#include <cstdint>
+//#include <boost/phoenix/object/construct.hpp>
 #include <boost/lambda/lambda.hpp>
 #include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
 #include <boost/spirit/include/support_multi_pass.hpp>
 
 namespace nlp {
@@ -14,12 +17,17 @@ public:
     explicit Grammar(std::ostream & out)
         : Grammar::base_type(m_unstructured_text_), m_out_(out)
     {
-        m_unstructured_text_ = *boost::spirit::ascii::char_[m_out_ << boost::lambda::_1];
+        m_structured_number_ = boost::spirit::ascii::no_case["one"] [boost::spirit::qi::_val = 1];
+
+        m_unstructured_text_ = *(m_structured_number_ [m_out_ << boost::spirit::qi::_1]
+                | boost::spirit::ascii::char_ [m_out_ << boost::lambda::_1]
+            );
     }
 
 private:
     std::ostream & m_out_;
     boost::spirit::qi::rule<IteratorIn> m_unstructured_text_;
+    boost::spirit::qi::rule<IteratorIn, uint64_t()> m_structured_number_;
 
 };
 
