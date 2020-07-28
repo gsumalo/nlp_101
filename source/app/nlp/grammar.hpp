cefcbb2eb3aaaec588bd8d003ad3ba5277c06a19
Jesus Malo Poyatos <jmalopoy@opentext.com>
2020-07-28 19:13:40 +0200

    Addition of rules for simple dozens

--- source/app/nlp/grammar.hpp
+++ source/app/nlp/grammar.hpp
@@ -72,12 +72,41 @@ public:
                 | m_natural_units_
             );
 
+        m_dozens_to_20_ %= (boost::spirit::ascii::no_case[tokens::ten] [boost::spirit::qi::_val = 10]
+                | boost::spirit::ascii::no_case[tokens::eleven]        [boost::spirit::qi::_val = 11]
+                | boost::spirit::ascii::no_case[tokens::twelve]        [boost::spirit::qi::_val = 12]
+                | boost::spirit::ascii::no_case[tokens::thirteen]      [boost::spirit::qi::_val = 13]
+                | boost::spirit::ascii::no_case[tokens::fourteen]      [boost::spirit::qi::_val = 14]
+                | boost::spirit::ascii::no_case[tokens::fifteen]       [boost::spirit::qi::_val = 15]
+                | boost::spirit::ascii::no_case[tokens::sixteen]       [boost::spirit::qi::_val = 16]
+                | boost::spirit::ascii::no_case[tokens::seventeen]     [boost::spirit::qi::_val = 17]
+                | boost::spirit::ascii::no_case[tokens::eighteen]      [boost::spirit::qi::_val = 18]
+                | boost::spirit::ascii::no_case[tokens::nineteen]      [boost::spirit::qi::_val = 19]
+            );
+
+        m_dozens_from_20_simple_ %= (boost::spirit::ascii::no_case[tokens::twenty] [boost::spirit::qi::_val = 20]
+                | boost::spirit::ascii::no_case[tokens::thirty]                    [boost::spirit::qi::_val = 30]
+                | boost::spirit::ascii::no_case[tokens::fourty]                    [boost::spirit::qi::_val = 40]
+                | boost::spirit::ascii::no_case[tokens::fifty]                     [boost::spirit::qi::_val = 50]
+                | boost::spirit::ascii::no_case[tokens::sixty]                     [boost::spirit::qi::_val = 60]
+                | boost::spirit::ascii::no_case[tokens::seventy]                   [boost::spirit::qi::_val = 70]
+                | boost::spirit::ascii::no_case[tokens::eighty]                    [boost::spirit::qi::_val = 80]
+                | boost::spirit::ascii::no_case[tokens::ninety]                    [boost::spirit::qi::_val = 90]
+            );
+
+        m_dozens_from_20_ %= m_dozens_from_20_simple_;
+
+        m_all_dozens_ %= (m_dozens_to_20_
+                | m_dozens_from_20_
+            );
+
         m_one_billion_ = (m_one_unit_ [boost::spirit::qi::_val = boost::spirit::qi::_1]
                 >> +(boost::spirit::ascii::blank)
                 >> boost::spirit::ascii::no_case[tokens::billion] [boost::spirit::qi::_val *= 1000000000]
             );
 
         m_structured_number_ %= (m_one_billion_
+                | m_all_dozens_
                 | m_all_units_
             );
 
@@ -90,7 +119,8 @@ private:
     std::ostream & m_out_;
     boost::spirit::qi::rule<IteratorIn> m_unstructured_text_;
     boost::spirit::qi::rule<IteratorIn, uint64_t()> m_zero_unit_, m_one_unit_, m_other_units_, m_natural_units_, 
-            m_all_units_, m_one_billion_, m_structured_number_;
+            m_all_units_, m_dozens_to_20_, m_dozens_from_20_simple_, m_dozens_from_20_, m_all_dozens_, 
+            m_one_billion_, m_structured_number_;
 
 };
 
